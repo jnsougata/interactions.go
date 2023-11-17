7ede449f5f084a5eba26cf6139765a7080b27ac1
sougata <jnsougata@gmail.com>
2023-11-17 23:09:37 +0530

    Added Embed

---
+++ embed.go
@@ -0,0 +1,52 @@
+package main
+
+type EmbedAuthor struct {
+	Name     string `json:"name"`
+	URL      string `json:"url,omitempty"`
+	IconURL  string `json:"icon_url,omitempty"`
+	ProxyURL string `json:"proxy_icon_url,omitempty"`
+}
+
+type EmbedFooter struct {
+	Text    string `json:"text"`
+	IconURL string `json:"icon_url,omitempty"`
+}
+
+type EmbedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline,omitempty"`
+}
+
+type EmbedImage struct {
+	URL    string `json:"url"`
+	Width  int    `json:"width,omitempty"`
+	Height int    `json:"height,omitempty"`
+}
+
+type EmbedThumbnail struct {
+	URL    string `json:"url"`
+	Width  int    `json:"width,omitempty"`
+	Height int    `json:"height,omitempty"`
+}
+
+type Embed struct {
+	Title       string         `json:"title,omitempty"`
+	Description string         `json:"description,omitempty"`
+	Color       int            `json:"color,omitempty"`
+	URL         string         `json:"url,omitempty"`
+	Timestamp   string         `json:"timestamp,omitempty"`
+	Author      EmbedAuthor    `json:"author,omitempty"`
+	Thumbnail   EmbedThumbnail `json:"thumbnail,omitempty"`
+	Image       EmbedImage     `json:"image,omitempty"`
+	Footer      EmbedFooter    `json:"footer,omitempty"`
+	Fileds      []EmbedField   `json:"fields,omitempty"`
+}
+
+func (e *Embed) AppendField(name, value string, inline bool) {
+	e.Fileds = append(e.Fileds, EmbedField{
+		Name:   name,
+		Value:  value,
+		Inline: inline,
+	})
+}
--- response.go
+++ response.go
@@ -1,11 +1,11 @@
 package main
 
 type SendingPayload struct {
-	TTS             bool   `json:"tts,omitempty"`
-	Content         string `json:"content,omitempty"`
-	Embeds          any    `json:"embeds,omitempty"`
-	AllowedMentions any    `json:"allowed_mentions,omitempty"`
-	Flags           int    `json:"flags,omitempty"`
-	Components      any    `json:"components,omitempty"`
-	Attchments      []File `json:"attachments,omitempty"`
+	TTS             bool    `json:"tts,omitempty"`
+	Content         string  `json:"content,omitempty"`
+	Embeds          []Embed `json:"embeds,omitempty"`
+	AllowedMentions any     `json:"allowed_mentions,omitempty"`
+	Flags           int     `json:"flags,omitempty"`
+	Components      any     `json:"components,omitempty"`
+	Attchments      []File  `json:"attachments,omitempty"`
 }
